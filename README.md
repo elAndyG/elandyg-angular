6c970a24c88c9f1a9c49a6b235adeb694a49f73f
ElAndyG <andrewgagne@outlook.com>
2019-07-02 21:22:59 -0400

    swap github.io back to original

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-[![Build Status](https://travis-ci.org/elAndyG/elandyg.github.io.svg?branch=master)](https://travis-ci.org/elAndyG/elandyg.github.io)
+[![Build Status](https://travis-ci.org/elAndyG/elandyg-angular.svg?branch=master)](https://travis-ci.org/elAndyG/elandyg-angular)
 
 # Andrew Gagne - Angular Resume
--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/elandyg.github.io'),
+      dir: require('path').join(__dirname, './coverage/elandyg-angular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
