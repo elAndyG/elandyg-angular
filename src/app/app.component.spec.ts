c367719ec04ec3dd5635b438b9b75a95af90f564
elandyg <andrewgagne@outlook.com>
2019-12-04 19:54:14 -0500

    clear tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,8 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ExperienceComponent } from './work-history/experience/experience.component';
+import { WorkHistoryComponent } from './work-history/work-history.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -9,7 +11,9 @@ describe('AppComponent', () => {
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ExperienceComponent,
+        WorkHistoryComponent
       ],
     }).compileComponents();
   }));
