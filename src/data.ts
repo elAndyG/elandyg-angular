ffccae6bf03bf685f0501bd22579e8582f94800c
ElAndyG <andrewgagne@outlook.com>
2019-12-06 22:50:22 -0500

    change wording of experiences

--- src/data.ts
+++ src/data.ts
@@ -12,15 +12,15 @@ export const experiences: Experience[] = [
     timeSpan: 'May 2018 - Present',
     description:
       // tslint:disable-next-line:max-line-length
-      'Rebuilt Marlin\'s flagship content player to dynamically display videos, images, html, twitter feeds and more from any location. Led the team in building a content manager for creating, editing and scheduling content.',
-    technologies: 'Angular 8, .Net Core, Sql Server'
+      'Leading a team of 5 in delivering digital workplace-safety media while designing a clean and easy to maintain code-base and development infrastructure.',
+    technologies: 'Angular 8, .Net Core, Sql Server, Azure'
   },
   {
     company: 'Insurity',
     title: 'Senior Software Engineer, Technical Lead',
     description:
       // tslint:disable-next-line: max-line-length
-      'Led a small team in building an internal code editor along with simplified workflows for on-boarding new clients and managing bulk actions. Engineered a ground up rebuild of an internal Rules Based Engine. Doubled as a Scrum Master. Ran code camps for the developer community to train TDD, OOP, SOLID principles and coding patterns.',
+      'Wearing many hats such as a senior level engineer, scrum master and technical lead, my role as a change-agent saw the introduction of test-driven development, continuous integration strategies and agile practices to help developers support numerous Fortune 500 companies.',
     timeSpan: '2014 - 2018',
     technologies: 'Angular 5, .Net Core, .Net, Sql Server'
   },
@@ -29,18 +29,18 @@ export const experiences: Experience[] = [
     title: 'Software Engineer II',
     description:
       // tslint:disable-next-line: max-line-length
-      ' Engineered code scaffolding for ramping up new projects including automatic updates for Account Administrators using a custom built dashboard. Enhanced security of the flagship product using RSA 2FA.',
+      'During my time as an engineer, I focused on developer efficiency, quality and automation. I then closed out my tenure by implementing additional user security to the flagship product.',
     timeSpan: 'Aug 2012 - Feb 2014',
-    technologies: '.Net, Sql Server, RSA, mustache.js, jQuery'
+    technologies: '.Net, Sql Server, RSA, mustache.js, jQuery, MVC, TCP'
   },
   {
     company: 'Origin Healthcare Solutions',
     title: 'Software Developer (Junior to Senior)',
     description:
       // tslint:disable-next-line: max-line-length
-      'Engineered a full service E-Prescribing system to communicate with pharmacies across the united states. Interfaced with a 3rd party Patient Portal to automate mapping of user inputs to practice charts. Created a notification suite to proactively alert support of issues within a client\'s solution.',
+      'Growing from a junior to a senior in my 7 years, I helped create an amazing EMR for medical groups and hospitals across the US while building my skills in desktop and database development.',
     timeSpan: 'Apr 2005 - Jul 2012',
-    technologies: ''
+    technologies: 'VB.Net, SQL Server, Infragistics, WCF, MVC, WinForms'
   },
   {
     company: 'Healthcare IQ',
@@ -49,7 +49,7 @@ export const experiences: Experience[] = [
       // tslint:disable-next-line: max-line-length
       'Created data entry systems for medical coders to have data scrubbed for errors legal violations prior to submission to insurance companies. Created an Electronic Medical Record system to help doctors store and regulate their data.',
     timeSpan: 'Jan 2001 - Apr 2005',
-    technologies: ''
+    technologies: 'VB.Net, SQL Server, Infragistics'
   }
 ];
 
